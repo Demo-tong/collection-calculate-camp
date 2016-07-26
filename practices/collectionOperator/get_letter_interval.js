255fa35a84d51353f24e76644104494237a1985f
Demo-tong <18829292377@163.com>
2016-07-26 11:57:12 +0800

    get letter from number

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -3,18 +3,22 @@
 function get_letter_interval(number_a, number_b) {
   //在这里写入代码
   let result = [];
+  let temp = [];
 
-  if(number_a < number_b){
-    for(let i = number_a; i <= number_b; ++i){
-      result.push(i);
+  if (number_a < number_b) {
+    for (let i = number_a; i <= number_b; ++i) {
+      result.push(97 + i - 1);
     }
-  }else{
-    for(let i = number_a ;i >= number_b; --i){
-      result.push(i);
+  } else {
+    for (let i = number_a; i >= number_b; --i) {
+      result.push(97 + i - 1);
     }
   }
 
-  return result;
+  temp = result.map(function (item) {
+    return String.fromCharCode(item);
+  });
+  return temp;
 }
 
 module.exports = get_letter_interval;
