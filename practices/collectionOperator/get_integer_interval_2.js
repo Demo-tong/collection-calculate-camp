b5ff4d021b0af0393ccdba9982d838d750c9a6f5
Demo-tong <18829292377@163.com>
2016-07-26 10:53:45 +0800

    tongyang get even numbers

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -3,9 +3,23 @@
 function get_integer_interval_2(number_a, number_b) {
   //在这里写入代码
 
-  if(number_a%2 === 0 && number_b%2 === 0){
-    
+  let result = [];
+
+  if (number_a < number_b) {
+    for (let i = number_a; i <= number_b; ++i) {
+      if (i % 2 === 0) {
+        result.push(i);
+      }
+    }
+  }else{
+    for (let i = number_a; i >= number_b; --i) {
+      if (i % 2 === 0) {
+        result.push(i);
+      }
+    }
   }
+  
+  return result;
 }
 
 module.exports = get_integer_interval_2;
